395552b21c081a1c8987fbe1048ffe0a6195c751
rudragondaliya <gondaliyarudra61@gmail.com>
2025-01-30 12:54:50 +0530

    done

---
+++ variables.js
@@ -0,0 +1,61 @@
+// // Number
+
+// let age = 25;
+// const pi = 3.14
+// console.log( typeof pi ,pi);
+// console.log( typeof age ,age);
+
+// // strings
+// let str = "Hello World!";
+// console.log(typeof str,str);
+// let userName = "Rudra";
+// console.log(typeof userName,userName);
+
+// // Boolean
+// let IsStudent = true;
+// console.log(typeof IsStudent,IsStudent);
+// let forSale = false;
+// console.log(typeof forSale,forSale);
+
+// // Null
+
+// let address = null;
+// console.log(typeof address,address);
+
+// // Undefined
+
+// let go;
+// console.log(typeof go);
+
+// // Object
+
+// let person = {name: 'Rudra',age: 20};
+// console.log(typeof person,person)
+
+
+// ARITHMETIC OPERATORS
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a**b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(a--);
+
+// ASSIGNMENT OPERATORS
+
+let x = 20;
+let y = 10;
+
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x**=y);
+console.log(x%=y);
+console.log(x+=y);
