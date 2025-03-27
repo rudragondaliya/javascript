6749228379d0bd06eac900bca7ce62f6a0c6d7df
rudragondaliya <gondaliyarudra61@gmail.com>
2025-03-27 18:13:39 +0530

    d

--- practice/localstorage/script.js
+++ practice/localstorage/script.js
@@ -2,6 +2,7 @@ let username = document.getElementById("username");
 let password = document.getElementById("password");
 let form = document.getElementById("myform");
 let showData = document.querySelector('#table tbody');
+let btn = document.getElementById("form-btn");
 let users = [];
 username.focus()
 
@@ -33,9 +34,28 @@ const display = ()=> {
            <td>${index +1}</td>
            <td>${user.username}</td>
            <td>${user.password}</td>
+           <td>
+            <button onclick ="editUser(${index})" class="btn btn-success">Edit</button>
+            <button onclick ="deleteUser(${index})" class=" btn btn-danger">Delete</button>
+            </td>
         `;
         showData.append(row);
     });
 };
 
 display();
+
+const deleteUser = (index) =>{
+ users.splice(index,1);
+ display();
+}
+ 
+const editUser = (index)=>{
+   let info = users.filter((_,idx)=> idx == index)[0];
+   username.value = info.username;
+   password.value = info.password;
+   btn.innerText = "Update";
+   btn.classList.add("btn-success");
+   btn.classList.remove("btn-primary");
+  
+}
