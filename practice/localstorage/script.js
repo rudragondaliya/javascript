6b9f25de17af6fc74178662e398c95dc301702a7
rudragondaliya <gondaliyarudra61@gmail.com>
2025-03-28 14:39:37 +0530

    d

--- practice/localstorage/script.js
+++ practice/localstorage/script.js
@@ -1,61 +1,88 @@
-let username = document.getElementById("username");
-let password = document.getElementById("password");
+
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let Phone = document.getElementById("Phone");
+let showData = document.querySelector("#table tbody");
 let form = document.getElementById("myform");
-let showData = document.querySelector('#table tbody');
-let btn = document.getElementById("form-btn");
+let btn = document.getElementById("formBtn");
+let editId = -1;
 let users = [];
-username.focus()
+firstName.focus();
 
 form.addEventListener('submit',(event) =>{
     event.preventDefault();
 
     let obj = {
-        username: username.value,
-        password: password.value,
-    };
+        firstName: firstName.value,
+        lastName:  lastName.value,
+        Phone: Phone.value
+    }
 
-    users.push(obj);
+    if(editId == -1){
+        users.push(obj);
 
-      username.value = '';
-      password.value = '';
-      username.focus();
+    }else{
 
+        users[editId] = obj;
+        editId = -1;
+    }
+
+    firstName.focus();
+   
+    resetForm();
     display();
 });
 
 const display = ()=> {
 
-    showData.innerHTML = '';
+        showData.innerHTML = '';
+    
+        users.forEach((user,index) => {
+            let row = document.createElement('tr');
+    
+            row.innerHTML = `
+               <td>${index +1}</td>
+               <td>${user.firstName}</td>
+               <td>${user.lastName}</td>
+               <td>${user.Phone}</td>
+               <td>
+                <button onclick ="editUser(${index})" class="btn btn-success">Edit</button>
+                <button onclick ="deleteUser(${index})" class=" btn btn-danger">Delete</button>
+                </td>
+            `;
+            showData.append(row);
+        });
+    };
 
-    users.forEach((user,index) => {
-        let row = document.createElement('tr');
-
-        row.innerHTML = `
-           <td>${index +1}</td>
-           <td>${user.username}</td>
-           <td>${user.password}</td>
-           <td>
-            <button onclick ="editUser(${index})" class="btn btn-success">Edit</button>
-            <button onclick ="deleteUser(${index})" class=" btn btn-danger">Delete</button>
-            </td>
-        `;
-        showData.append(row);
-    });
-};
-
-display();
-
+    display();
+ 
 const deleteUser = (index) =>{
  users.splice(index,1);
  display();
 }
- 
+
+
 const editUser = (index)=>{
-   let info = users.filter((_,idx)=> idx == index)[0];
-   username.value = info.username;
-   password.value = info.password;
-   btn.innerText = "Update";
-   btn.classList.add("btn-success");
-   btn.classList.remove("btn-primary");
-  
+let info = users.filter((_,idx)=> idx == index)[0];
+firstName.value = info.firstName;
+lastName.value = info.lastName;
+Phone.value = info.Phone;
+btn.innerText = "Update";
+btn.classList.add("btn-success");
+btn.classList.remove("btn-primary");
+
+editId = index;
+
+}
+
+function resetForm(){
+
+    firstName.value = "";
+    lastName.value = "";
+    Phone.value = "";
+    formBtn.textContent = "Register";
+    formBtn.classList.add("btn-primary");
+    formBtn.classList.remove("btn-success");
+    firstName.focus();
+
 }
