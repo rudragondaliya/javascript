c204587968c0a498092f40d6bad427dc581f69ca
rudragondaliya <gondaliyarudra61@gmail.com>
2025-04-01 19:07:39 +0530

    d

--- practice/student-record/script.js
+++ practice/student-record/script.js
@@ -1,7 +1,9 @@
 const firstName = document.getElementById("firstname");
 const lastName = document.getElementById("lastname");
 let form =  document.getElementById("form");
+let showData = document.querySelector("#table tbody")
 let users = [];
+let editId = -1
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
@@ -11,8 +13,44 @@ form.addEventListener("submit",(e)=>{
         lastName: lastName.value,
     };
 
-    console.log(obj)
+     users.push(obj);
+     firstName.value = '';
+     lastName.value = '';
 
+     display();
 
+});
 
-})
+const display = ()=>{
+
+    showData.innerHTML = '';
+    
+    users.forEach((user,index)=>{
+        let row = document.createElement("tr");
+
+        row.innerHTML =
+        `
+        <td>${index+1}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>
+        <button onclick="deleteUser(`${index}`)" class="btn btn-danger">Delete</button>
+        <button onclick="editUser()" class="btn btn-warning">Edit</button>
+        </td>
+        `;
+        showData.append(row);
+    })
+}
+
+const deleteUser = (index) =>
+{
+    users.splice(index,1);
+    display();
+}
+
+const editUser = (index) => {
+    editId = index;
+    let users = user[index];
+    firstName = user.firstName;
+    lastName = user.lastName;
+}
